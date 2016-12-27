2b2c79da095ef9992ca9d94a75dbfb1562e897e1
Ali Moghiseh <moghiseha@gmail.com>
2016-12-27 09:59:02 +0100

    *Add* second model

--- webmwf/admin.py
+++ webmwf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import one_x_one
+from .models import one_x_one, calc_time
+from .models import calc_time
 # Register your models here.
 
 admin.site.register(one_x_one)
+admin.site.register(calc_time)
--- webmwf/models.py
+++ webmwf/models.py
@@ -8,3 +8,12 @@ class one_x_one(models.Model):
     qs   = models.CharField(max_length=250)
     ans  = models.IntegerField()
     user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.qs,self.user)
+
+class calc_time(models.Model):
+    qs = models.CharField(max_length=250)
+    date = models.DateTimeField()
+    user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.qs,self.date)
